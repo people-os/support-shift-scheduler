50e355285bb27504ff8dafa0dda54f77ce26e5c8
AlidaOdendaal <alida@balena.io>
2019-12-02 14:48:03 +0200

    Apply various updates after second review round
    Change-type: patch
    Signed-off-by: AlidaOdendaal <alida@balena.io>
    Connects-to: #2

--- helper-scripts/beautify-schedule.js
+++ helper-scripts/beautify-schedule.js
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const dateformat = require('dateformat')
-const fs = require('fs')
-const _ = require('lodash')
+const dateformat = require('dateformat');
+const _ = require('lodash');
+const fs = require('mz/fs');
 
 /**
  * Convert array from Next Cycle Dates Google Sheet into object.
@@ -23,66 +23,82 @@ const _ = require('lodash')
  * @return {string}          Formatted like like Monday, November 18th
  */
 function prettyDateStr(date) {
-  return dateformat(date, "dddd, mmmm dS")
+	return dateformat(date, 'dddd, mmmm dS');
 }
 
 /**
  * Write beautified schedule, as well as Flowdock message, to text files.
  * @param  {object}   scheduleJSON   Scheduling algorithm output object (read from file)
  */
-function writePrettifiedText(scheduleJSON) {
-  // Write pretty schedule, to be used for sanity check:
-  const agentHours = {}
-  let prettySchedule = ""
+async function writePrettifiedText(scheduleJSON) {
+	// Write pretty schedule, to be used for sanity check:
+	const agentHours = {};
+	let prettySchedule = '';
 
-  for (const epoch of scheduleJSON) {
-    //let startDate = new Date(epoch.start_date)
-    prettySchedule += `\nShifts for ${prettyDateStr(epoch.start_date)}\n`
+	for (const epoch of scheduleJSON) {
+		//let startDate = new Date(epoch.start_date)
+		prettySchedule += `\nShifts for ${epoch.start_date}\n`;
 
-    for (const shift of epoch.shifts) {
-      const agentName = shift.agent.replace(/ <.*>/, '')
-      const len = shift.end - shift.start
-      const startStr = `${_.padStart(shift.start, 2, '0')}:00`
-      const endStr = `${_.padStart(shift.end, 2, '0')}:00`
-      prettySchedule += `${startStr} - ${endStr} (${len} hours) - ${agentName}\n`
-      agentHours[agentName] = agentHours[agentName] || 0
-      agentHours[agentName] += len
-    }
-  }
-  prettySchedule += `\n#rollcall\n\n`
-  prettySchedule += 'Support hours\n-------------\n'
+		for (const shift of epoch.shifts) {
+			const agentName = shift.agent.replace(/ <.*>/, '');
+			const len = shift.end - shift.start;
+			const startStr = `${_.padStart(shift.start, 2, '0')}:00`;
+			const endStr = `${_.padStart(shift.end, 2, '0')}:00`;
+			prettySchedule += `${startStr} - ${endStr} (${len} hours) - ${agentName}\n`;
+			agentHours[agentName] = agentHours[agentName] || 0;
+			agentHours[agentName] += len;
+		}
+	}
+	prettySchedule += `\n#rollcall\n\n`;
+	prettySchedule += 'Support hours\n-------------\n';
 
-  let agentHoursList = _.map(agentHours, (hours, handle) => { handle = handle.replace(/ <.*>/, ''); return { handle, hours }})
-  agentHoursList = _.sortBy(agentHoursList, (agent) => { return agent.hours }).reverse()
+	let agentHoursList = _.map(agentHours, (hours, handle) => {
+		handle = handle.replace(/ <.*>/, '');
+		return { handle, hours };
+	});
+	agentHoursList = _.sortBy(agentHoursList, agent => {
+		return agent.hours;
+	}).reverse();
 
-  for (const agent of agentHoursList) {
-    const handle = agent.handle.replace(/@/, '').replace(/ <.*>/, '')
-    prettySchedule += `${handle}: ${agent.hours}\n`
-  }
-  fs.writeFile('beautified-schedule.txt', prettySchedule, 'utf8', err => {})
+	for (const agent of agentHoursList) {
+		const handle = agent.handle.replace(/@/, '').replace(/ <.*>/, '');
+		prettySchedule += `${handle}: ${agent.hours}\n`;
+	}
+	try {
+		await fs.writeFile('beautified-schedule.txt', prettySchedule);
+	} catch (e) {
+		console.error(e);
+	}
 
-  // Write Flowdock message, with which to ping agents to check their calendars:
-  let flowdockMessage = ""
-  flowdockMessage += `**Agents, please check your calendars for the support schedule for next week (starting on ${dateformat(scheduleJSON[0].start_date, "mmmm dS")}).**\n\n`
-  flowdockMessage += 'Please acknowledge, or let me know if you require any changes.\n'
-  flowdockMessage += `\n#rollcall\n\n`
+	// Write Flowdock message, with which to ping agents to check their calendars:
+	let flowdockMessage = '';
+	flowdockMessage += `**Agents, please check your calendars for the support schedule for next week (starting on ${scheduleJSON[0].start_date}).**\n\n`;
+	flowdockMessage +=
+		'Please acknowledge, or let me know if you require any changes.\n';
+	flowdockMessage += `\n#rollcall\n\n`;
 
-  for (const agent of agentHoursList) {
-    flowdockMessage += `${agent.handle}\n`
-  }
-  fs.writeFile('flowdock-message.txt', flowdockMessage, 'utf8', err => {})
+	for (const agent of agentHoursList) {
+		flowdockMessage += `${agent.handle}\n`;
+	}
+	try {
+		await fs.writeFile('flowdock-message.txt', flowdockMessage);
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 // Read scheduling algorithm output file name from command line:
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 if (args.length != 1) {
-  console.log(`Usage: node ${__filename} <path-to-support-shift-scheduler-output.json>`)
-  process.exit(1)
+	console.log(
+		`Usage: node ${__filename} <path-to-support-shift-scheduler-output.json>`
+	);
+	process.exit(1);
 }
 
 // Load JSON object from output file:
-const jsonPath = args[0]
-const jsonObject = JSON.parse(fs.readFileSync(jsonPath))
+const jsonPath = args[0];
+const jsonObject = JSON.parse(fs.readFileSync(jsonPath));
 
 // Write beautified-schedule.txt and flowdock-message.txt:
-writePrettifiedText(jsonObject)
+writePrettifiedText(jsonObject);
--- helper-scripts/download-and-configure-input.js
+++ helper-scripts/download-and-configure-input.js
@@ -13,49 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const _ = require('lodash')
-const fs = require('fs')
-const Promise = require('bluebird')
-const mkdirp = Promise.promisify(require('mkdirp'))
-const { getAuthClient } = require('../lib/gauth')
-const { getNextCycleDates } = require('../lib/gsheets')
-const { getSchedulerInput } = require('../lib/gsheets')
-const { validateJSONScheduleInput } = require('../lib/validate-json')
+const _ = require('lodash');
+const fs = require('mz/fs');
+const Promise = require('bluebird');
+const mkdirp = Promise.promisify(require('mkdirp'));
+const { getAuthClient } = require('../lib/gauth');
+const { getNextCycleDates } = require('../lib/gsheets');
+const { getSchedulerInput } = require('../lib/gsheets');
+const { validateJSONScheduleInput } = require('../lib/validate-json');
 
 const SCHEDULE_OPTS = {
-  "num_consecutive_days": 5,
-  "num_simultaneous_tracks": 2,
-  "support_start_hour": 8,
-  "support_end_hour": 24,
-  "shift_min_duration": 2,
-  "shift_max_duration": 8,
-  "optimization_timeout": 3600
-}
+	numConsecutiveDays: 5,
+	numSimultaneousTracks: 2,
+	supportStartHour: 8,
+	supportEndHour: 24,
+	shiftMinDuration: 2,
+	shiftMaxDuration: 8,
+	optimizationTimeout: 3600,
+};
 
 /**
  * Read and configure input data from Google Sheets, and save as JSON object
  */
 async function getData() {
-  try {
-    const auth = await getAuthClient()
-    console.log('Got auth token successfully')
+	try {
+		const auth = await getAuthClient();
+		const parsedNextCycleDates = await getNextCycleDates(auth);
+		const nextMondayDate = parsedNextCycleDates.support;
 
-    const parsedNextCycleDates = await getNextCycleDates(auth)
-    console.log(JSON.stringify(parsedNextCycleDates, null, 2))
-    const nextMondayDate = parsedNextCycleDates.support
+		const schedulerInput = await getSchedulerInput(
+			auth,
+			nextMondayDate,
+			SCHEDULE_OPTS.numConsecutiveDays
+		);
+		_.assign(schedulerInput.options, SCHEDULE_OPTS);
+		console.log(JSON.stringify(schedulerInput, null, 2));
+		const schedulerInputValidation = await validateJSONScheduleInput(
+			schedulerInput
+		);
 
-    const schedulerInput = await getSchedulerInput(auth, nextMondayDate, SCHEDULE_OPTS.num_consecutive_days)
-    _.assign(schedulerInput.options, SCHEDULE_OPTS)
-    console.log(JSON.stringify(schedulerInput, null, 2))
-    const schedulerInputValidation = await validateJSONScheduleInput(schedulerInput)
+		const fileDir = `./logs-${nextMondayDate}`;
+		await mkdirp(fileDir);
 
-    const fileDir = './logs-' + nextMondayDate
-    await mkdirp(fileDir)
-
-    fs.writeFile(fileDir + '/support-shift-scheduler-input.json', JSON.stringify(schedulerInput, null, 2), 'utf8', err => {})
-  } catch (e) {
-  console.error(e)
-  }
+		await fs.writeFile(
+			fileDir + '/support-shift-scheduler-input.json',
+			JSON.stringify(schedulerInput, null, 2)
+		);
+	} catch (e) {
+		console.error(e);
+	}
 }
 
-getData()
+getData();
