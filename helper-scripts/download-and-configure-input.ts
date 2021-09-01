bf987e21640a05e787d72a80ee56736c056ac4c4
AlidaOdendaal <alida@balena.io>
2021-09-01 22:20:50 +0200

    download-and-configure-input.ts: Fix for input file not being written
    Change-type: patch
    Signed-off-by: AlidaOdendaal <alida@balena.io>

--- helper-scripts/download-and-configure-input.ts
+++ helper-scripts/download-and-configure-input.ts
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 import * as _ from 'lodash';
-import * as fs from 'mz/fs';
-import * as Promise from 'bluebird';
 import * as mkdirp from 'mkdirp';
-const mkdirpAsync = Promise.promisify(mkdirp);
+
+const fs = require('fs').promises;
 
 import { getAuthClient } from '../lib/gauth';
 import { getSchedulerInput } from '../lib/gsheets';
@@ -27,23 +26,23 @@ import { validateJSONScheduleInput } from '../lib/validate-json';
  * Read and configure input data from Google Sheets, and save as JSON object
  */
 async function getData(startDate: string, supportName: string) {
-	const support = JSON.parse(
-		fs.readFileSync('helper-scripts/options/' + supportName + '.json', 'utf8'),
-	);
-
 	try {
+		const supportStr = await fs.readFile('helper-scripts/options/' + supportName + '.json', 'utf8')
+		const support = JSON.parse(supportStr);		
 		const auth = await getAuthClient(support);
 		const schedulerInput = await getSchedulerInput(auth, startDate, support);
 		_.assign(schedulerInput.options, support);
-		console.log(JSON.stringify(schedulerInput, null, 2));
+		const stringifiedInput = JSON.stringify(schedulerInput, null, 2)
+		console.log(stringifiedInput);
+
 		await validateJSONScheduleInput(schedulerInput);
 
 		const fileDir = `./logs/${startDate}_` + supportName;
-		await mkdirpAsync(fileDir, null);
-
+		await mkdirp(fileDir, null);
 		await fs.writeFile(
 			fileDir + '/support-shift-scheduler-input.json',
-			JSON.stringify(schedulerInput, null, 2),
+			stringifiedInput,
+			(err: any) => {console.log(err)}
 		);
 	} catch (e) {
 		console.error(e);
